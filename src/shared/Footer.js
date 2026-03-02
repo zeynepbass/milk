008c79fa78239a394071cee55c2179e7d49bcb75
zeynep bas <baszynpp@gmail.com>
2026-03-02 22:02:39 +0300

    .

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="bg-white border-t border-gray-200">
       <div className="max-w-7xl px-6 py-6 flex items-center gap-2 text-sm text-[rgb(71,92,120)]">
-        <span>© {new Date().getFullYear()} | Keyifle kodlanmıştır.</span>
+        <span>©{new Date().getFullYear()} | Keyifle kodlanmıştır.</span>
         <video
           src="/images/milk.mp4"
           autoPlay
