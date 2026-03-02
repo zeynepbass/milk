6e1dd6c8d8bc9f532a9df9eb3b3bda8614f9f9d8
zeynep bas <baszynpp@gmail.com>
2026-03-02 22:04:03 +0300

    .

--- src/shared/Container.js
+++ src/shared/Container.js
@@ -2,11 +2,9 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Outlet } from "react-router-dom";
 export function Container() {
-  
   return (
     <div className="container mx-auto">
       <Header />
-
       <Outlet />
       <Footer />
     </div>
